773db4b7b0e2e75dedaa2a84c44234c8ed60d860
georgelog10 <georgelog10@gmail.com>
2024-05-07 21:52:37 +0300

    commit

--- src/components/LoginSections/Login2.js
+++ src/components/LoginSections/Login2.js
@@ -55,7 +55,7 @@ const Login2 = () => {
         </div>
         <div className='d-flex justify-content-center gap-4'>
           <button className="py-2 px-2 rounded-3 border border-2 border-secondary-subtle mb-4 d-flex gap-3 justify-content-center text-body-secondary" onClick={logGoogleUser}><img src={GoogleLogo} alt='Google Logo' width={24} height={24}/></button>
-          <button className='py-2 px-2 ounded-3 border border-2 border-secondary-subtle mb-4 d-flex gap-3 justify-content-center text-body-secondary'><img src={GoogleLogo} alt='Google Logo' width={24} height={24}/></button>
+          <button className='py-2 px-2 rounded-3 border border-2 border-secondary-subtle mb-4 d-flex gap-3 justify-content-center text-body-secondary'><img src={GoogleLogo} alt='Google Logo' width={24} height={24}/></button>
           <button className='py-2 px-2 rounded-3 border border-2 border-secondary-subtle mb-4 d-flex gap-3 justify-content-center text-body-secondary'><img src={GoogleLogo} alt='Google Logo' width={24} height={24}/></button>
         </div>
         <p className='text-center text-dark register-link'>Nu ești membru? <Link to='/register'>Creează un cont</Link></p>
