fbb644b6437a3da9efc0cdaadc9b704b6f97dfbb
georgelog10 <georgelog10@gmail.com>
2024-05-07 22:22:31 +0300

    commit

--- src/components/LoginSections/Login2.js
+++ src/components/LoginSections/Login2.js
@@ -55,7 +55,7 @@ const Login2 = () => {
         </div>
         <div className='d-flex justify-content-center gap-4'>
           <button className="py-2 px-2 rounded-3 border border-2 border-secondary-subtle mb-4 d-flex gap-3 justify-content-center text-body-secondary" onClick={logGoogleUser}><img src={GoogleLogo} alt='Google Logo' width={24} height={24}/></button>
-          <button className='py-2 px-2 rounded-3 border border-2 mb-4 d-flex gap-3 justify-content-center text-body-secondary facebook-login'><FontAwesomeIcon icon={faFacebookF}/></button>
+          <button className='py-2 px-2 rounded-3 border border-2 mb-4 d-flex gap-3 justify-content-center text-body-secondary facebook-login'><FontAwesomeIcon icon={faFacebookF} className='text-white' width={24} height={24}/></button>
           <button className='py-2 px-2 rounded-3 border border-2 border-secondary-subtle mb-4 d-flex gap-3 justify-content-center text-body-secondary apple-login'><FontAwesomeIcon icon={faApple}/></button>
         </div>
         <p className='text-center text-dark register-link'>Nu ești membru? <Link to='/register'>Creează un cont</Link></p>
