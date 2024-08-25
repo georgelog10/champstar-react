4c7de813427a4f1dddfd47be3a00c42452a0c913
georgelog10 <georgelog10@gmail.com>
2024-08-25 17:04:54 +0300

    new dashboard

--- src/components/Dashboard/DashboardSidebar/DashboardSidebar.js
+++ src/components/Dashboard/DashboardSidebar/DashboardSidebar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../../assets/logo-removebg-preview.png'
+import Logo from '../../../assets/logo-removebg-preview.png'
 
 const DashboardSidebar = () => {
   return (
