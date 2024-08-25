a117230f6eac71d6ab78f424659bce90c687fc6a
georgelog10 <georgelog10@gmail.com>
2024-08-25 18:08:47 +0300

    new dashboard

--- src/components/Dashboard/DashboardSidebar/DashboardSidebar.js
+++ src/components/Dashboard/DashboardSidebar/DashboardSidebar.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Logo from '../../../assets/logo-removebg-preview.png'
 import DashboardIcon from '../../../assets/icons/dashboard-square-edit-stroke-rounded.svg'
+import ContestListIcon from '../../../assets/icons/task-01-stroke-rounded.svg'
+import BookmarkIcon from '../../../assets/icons/bookmark-03-stroke-rounded.svg'
+import NotificationsIcon from '../../../assets/icons/notification-02-stroke-rounded.svg'
+import ChatIcon from '../../../assets/icons/bubble-chat-stroke-rounded.svg'
+import SettingsIcon from '../../../assets/icons/settings-02-stroke-rounded.svg'
 import { Link } from 'react-router-dom'
 
 const DashboardSidebar = () => {
@@ -14,11 +19,26 @@ const DashboardSidebar = () => {
                 <img src={DashboardIcon} alt='Dashboard Icon'/>
                 Dashboard
             </Link>
-            <p>Concursuri</p>
-            <p>Concursurile mele</p>
-            <p>Notificari</p>
-            <p>Chat</p>
-            <p>Setari</p>
+            <Link className={`d-flex align-items-center gap-1`}>
+                <img src={ContestListIcon} alt='Dashboard Icon'/>
+                Concursuri
+            </Link>
+            <Link className={`d-flex align-items-center gap-1`}>
+                <img src={BookmarkIcon} alt='Dashboard Icon'/>
+                Concursurile mele
+            </Link>
+            <Link className={`d-flex align-items-center gap-1`}>
+                <img src={NotificationsIcon} alt='Dashboard Icon'/>
+                Notificări
+            </Link>
+            <Link className={`d-flex align-items-center gap-1`}>
+                <img src={ChatIcon} alt='Dashboard Icon'/>
+                Chat
+            </Link>
+            <Link className={`d-flex align-items-center gap-1`}>
+                <img src={SettingsIcon} alt='Dashboard Icon'/>
+                Setări
+            </Link>
         </div>
         <div className='dashboard-profile'>
             <img src=''/>
