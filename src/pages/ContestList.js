18b9adba7f6c4f7c32ef6b6567e1021344de9487
georgelog10 <georgelog10@gmail.com>
2024-09-07 00:44:42 +0300

    new dashboard

--- src/pages/ContestList.js
+++ src/pages/ContestList.js
@@ -6,6 +6,11 @@ import { BottomNavigation, BottomNavigationAction, Box } from "@mui/material";
 // import HomeIcon from '@mui/icons-material/Home';
 // import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import DashboardIcon from "../assets/icons/dashboard-square-edit-stroke-rounded.svg";
+import ContestsIcon from "../assets/icons/task-01-stroke-rounded.svg";
+import MyContestsIcon from "../assets/icons/bookmark-03-stroke-rounded.svg";
+import NotificationsIcon from "../assets/icons/notification-02-stroke-rounded.svg";
+import ChatIcon from "../assets/icons/bubble-chat-stroke-rounded.svg";
+import SettingsIcon from "../assets/icons/settings-02-stroke-rounded.svg";
 
 const Dashboard = () => {
   const [value, setValue] = React.useState(0);
@@ -35,6 +40,12 @@ const Dashboard = () => {
             }}
           >
             <BottomNavigationAction icon={<img src={DashboardIcon} alt="dashboard" style={{ width: 24, height: 24 }} />} />
+            <BottomNavigationAction icon={<img src={ContestsIcon} alt="contests" style={{ width: 24, height: 24 }} />} />
+            <BottomNavigationAction icon={<img src={MyContestsIcon} alt="my-contests" style={{ width: 24, height: 24 }} />} />
+            <BottomNavigationAction icon={<img src={NotificationsIcon} alt="notifications" style={{ width: 24, height: 24 }} />} />
+            <BottomNavigationAction icon={<img src={ChatIcon} alt="chat" style={{ width: 24, height: 24 }} />} />
+            <BottomNavigationAction icon={<img src={SettingsIcon} alt="settings" style={{ width: 24, height: 24 }} />} />
+
           </BottomNavigation>
         </Box>
       </section>
