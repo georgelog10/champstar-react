a0af4e59c06d8ba2948f670b16887f89ab87369f
georgelog10 <georgelog10@gmail.com>
2024-09-07 00:05:42 +0300

    new dashboard

--- src/pages/ContestList.js
+++ src/pages/ContestList.js
@@ -1,16 +1,12 @@
-import React from 'react'
-import DashboardNav from '../components/Dashboard/DashboardNav/DashboardNav'
-import DashboardFilter from '../components/Dashboard/DashboardFilter/DashboardFilter'
-import DashboardCards from '../components/Dashboard/DashboardCards/DashboardCards'
-import { BottomNavigation, BottomNavigationAction, Box } from '@mui/material';
-// import HomeIcon from '@mui/icons-material/Home';
-// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import FolderIcon from '@mui/icons-material/Folder';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import DashboardSidebar from '../components/Dashboard/DashboardSidebar/DashboardSidebar';
-import DashboardTopBar from '../components/Dashboard/DashboardTopBar/DashboardTopBar';
+import React from "react";
+import DashboardNav from "../components/Dashboard/DashboardNav/DashboardNav";
+import DashboardFilter from "../components/Dashboard/DashboardFilter/DashboardFilter";
+import DashboardCards from "../components/Dashboard/DashboardCards/DashboardCards";
+import { BottomNavigation, BottomNavigationAction, Box } from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DashboardSidebar from "../components/Dashboard/DashboardSidebar/DashboardSidebar";
+import DashboardTopBar from "../components/Dashboard/DashboardTopBar/DashboardTopBar";
 
 const Dashboard = () => {
   const [value, setValue] = React.useState(0);
@@ -18,34 +14,34 @@ const Dashboard = () => {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-  
+
   return (
     <>
-      <section id='contests'>
-        <div className='container-lg d-flex gap-5 py-4'>
-          <DashboardSidebar/>
-          <div className='w-75 dashboard-content'>
-            <DashboardTopBar/>
-            <DashboardCards/>
+      <section id="contests">
+        <div className="container-lg d-flex gap-5 py-4">
+          <DashboardSidebar />
+          <div className="w-75 dashboard-content">
+            <DashboardTopBar />
+            <DashboardCards />
           </div>
         </div>
       </section>
       <section id="mobile-app-bar">
-      <Box sx={{ width: '100vw' }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction icon={<HomeIcon />} />
-        <BottomNavigationAction icon={<AccountCircleIcon />} />
-      </BottomNavigation>
-    </Box>
-    </section>
-  </>
-  )
-}
+        <Box sx={{ width: "100vw" }}>
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          >
+            <BottomNavigationAction icon={<HomeIcon />} />
+            <BottomNavigationAction icon={<AccountCircleIcon />} />
+          </BottomNavigation>
+        </Box>
+      </section>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
